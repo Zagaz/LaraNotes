000949960ef4bd6b98046702cf3c7812e89201a3
Zagaz <andre_ranulfo@hotmail.com>
2024-09-08 17:23:38 -0300

    Refactor AuthController to handle form submission in loginSubmit method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,10 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo "Form submitted successfully";
+        echo $request->input('text_username');
+        echo "<br>";
+        echo $request->input('text_password');
+        //       echo "Form submitted successfully";
     }
     public function logout()
     {
