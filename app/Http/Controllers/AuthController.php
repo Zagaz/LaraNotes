4ad70d1dabc8bded74054d53c7a14e8a9600720e
Zagaz <andre_ranulfo@hotmail.com>
2024-09-08 17:41:03 -0300

    Refactor AuthController to handle form submission in loginSubmit method and improve form validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,10 +13,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo "<br>";
-        echo $request->input('text_password');
-        //       echo "Form submitted successfully";
+        //form validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+
+            ]
+        );
+        // Get user input
+        $username = $request->input('text_username');
+        $password =  $request->input('text_password');
+        echo "OK";
+
     }
     public function logout()
     {
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,12 +19,14 @@
                                 <div class="mb-3">
                                     <label for="text_username" class="form-label">Username</label>
                                     <input type="text" class="form-control bg-dark text-info" name="text_username"
-                                        required>
+                                        {{-- required --}}
+                                        >
                                 </div>
                                 <div class="mb-3">
                                     <label for="text_password" class="form-label">Password</label>
                                     <input type="password" class="form-control bg-dark text-info" name="text_password"
-                                        required>
+                                        {{-- required --}}
+                                        >
                                 </div>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
@@ -37,6 +39,17 @@
                     <div class="text-center text-secondary mt-3">
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
+                    {{---Errors---}}
+                    @if($errors->any())
+                        <div class="alert alert-danger mt-3">
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                    @endif
 
                 </div>
             </div>
