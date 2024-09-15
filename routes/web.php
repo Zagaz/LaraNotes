06a3d59af93210929bb4ca9fe813ffb307f166cf
Zagaz <andre_ranulfo@hotmail.com>
2024-09-14 22:42:02 -0300

    Refactor routes and add middleware for checking user login status

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,17 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLog;
 use Illuminate\Support\Facades\Route;
 
 //Auth routes
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/loginSubmit',[AuthController::class,'loginSubmit']);
 
+Route::middleware([CheckIsLog::class])->group(function () {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newnote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
 
-Route::get('/', [MainController::class, 'index']);
-Route::get('/newnote', [MainController::class, 'newNote']);
-Route::get('/logout', [AuthController::class, 'logout']);
+
