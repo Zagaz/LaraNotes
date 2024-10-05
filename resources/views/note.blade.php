728ae58b7ba55bcd85ca58934282411fdb9a5819
Zagaz <andre_ranulfo@hotmail.com>
2024-10-05 13:44:48 -0300

    Refactor note.blade.php to update delete button link encryption

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -10,7 +10,7 @@
                 <div class="col text-end">
                     <a href="/edit/{{ Crypt::encrypt($note['id']) }}"
                         class="btn btn-outline-secondary btn-sm mx-1"><i class="fa-regular fa-pen-to-square"></i></a>
-                    <a href="/delete/{{ $note['id'] }}" class="btn btn-outline-danger btn-sm mx-1"><i
+                    <a href="/delete/{{ Crypt::encrypt($note['id']) }}" class="btn btn-outline-danger btn-sm mx-1"><i
                             class="fa-regular fa-trash-can"></i></a>
                 </div>
             </div>
